96bf824e7f795d50ad05bf73606d675508a49d0e
violangige <violangige@gmail.com>
2022-08-09 23:57:31 +0300

    updated

--- alpha.c
+++ alpha.c
@@ -1,7 +1,12 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * include comment
+ * print_string - loops through a string and prints
+ * every character
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: number of char printed
  */
 int print_string(va_list l, flags_t *f)
 {
